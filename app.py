e73e2e79786a3301d6dafaf143a4158410a493b1
Rahman ziad <mrahmanziad@zohomail.com>
2025-06-24 18:47:05 +0000

    third

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def upload():
         return jsonify({'success': False, 'message': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
